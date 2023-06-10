5b3551e58b8e5123881a7cbc329fe421a832220c
ObadaAbdAlkhaleq <83885520+ObadaAbdAlkhaleq@users.noreply.github.com>
2023-06-11 02:50:33 +0300

    Medium | 238. Product of Array Except Self | solved O(n) and O(n^2)

--- medium.py
+++ medium.py
@@ -76,3 +76,48 @@ Output: [1]
 # print(topKFrequent(nums=[1, 1, 1, 2, 2, 4, 5, 5, 8, 8, 8, 8, 3, 7, 7, 7], k=2))
 
 #################################################################################################
+
+"""
+238. Product of Array Except Self
+Given an integer array nums, return an array answer 
+such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+You must write an algorithm that runs in O(n) time and without using the division operation.
+
+Example 1:
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+
+Example 2:
+Input: nums = [-1,1,0,-3,3]
+Output: [0,0,9,0,0]
+"""
+# def productExceptSelf(nums: list[int]) -> list[int]:
+    # O(n^2)
+	# res = []
+	# pr, pl, product = 1, 1, 1
+	# for i in range(len(nums)):
+	# 	for l in range(0, i):
+	# 		pl *= nums[l]
+	# 	for r in range(len(nums)-1, i, -1):
+	# 		pr *= nums[r]
+	# 	product = pr * pl
+	# 	res.append(product)
+	# 	product, pl, pr = 1, 1, 1
+	# return res
+	# # O(n)
+# 	res = [1] * len(nums)
+# 	pr, pl = 1, 1
+# 	for l in range(len(nums)):
+# 		res[l] = pl
+# 		pl *= nums[l]
+# 	for r in range(len(nums)-1, -1, -1):
+# 		res[r] = pr
+# 		pr *= nums[r]
+# 	return res
+
+# print(productExceptSelf(nums=[1, 2, 3, 4]))
+
+#################################################################################################
